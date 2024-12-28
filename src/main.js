aff85ea39687c22ccb95e4d7c05285405319c233
tim <tfangmeyer@gmail.com>
2024-12-28 13:15:59 +0100

    Externalized css into a single style.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import './assets/styles.css'
 
 // Bulma CSS is a modern CSS framework based on Flexbox. 
 // Import Bulma CSS
